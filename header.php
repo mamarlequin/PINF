af0298e1e5f86406610666672cf0ef2945b2a369
Marceau <mamar123095@gmail.com>
2026-01-07 16:41:23 +0100

    premier commit

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FabLab</title>
+    
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        body { font-family: 'Inter', sans-serif; background-color: #f8fafc; }
+
+    </style>
+</head>
+<body class="text-slate-800 mx-20">
+    <nav class="max-w-6xl mx-auto mt-10 px-4 sm:px-6 lg:px-8 sticky top-4 my-10 z-50">
+        <div class="bg-slate-300 rounded-full border shadow-sm px-6 flex items-center py-2 justify-between h-1-">
+                <a href="index.php" class="flex items-center gap-2">
+                        LOGOLOGOLOGOLOGO
+                </a>
+                
+                <div class="hidden md:flex items-center gap-14 text-sm-bold font-medium ">
+                    <a href="reserver.php" class="transition-colors hover:text-indigo-600">Réserver</a>
+                    <a href="machines.php" class="transition-colors hover:text-indigo-600">Machines</a>
+                    
+                    <a href="login.php" class="bg-indigo-600 text-white px-5 py-2 rounded-3xl hover:bg-indigo-700 transition-all shadow-sm active:scale-95">
+                        Connexion
+                    </a>
+                </div>
+        </div>
+    </nav>
---
+++ main.php
@@ -0,0 +1,25 @@
+<div class="max-w-6xl mx-auto px-6 mt-16">
+    <div class="p-8 md:p-12 flex flex-col md:flex-row items-center justify-between gap-20">
+        <div class="flex flex-col"> 
+            <div class="mb-8"> 
+                <h1 class="text-4xl md:text-5xl font-extrabold text-slate-900 leading-tight">
+                    Réservez votre <span class="text-indigo-600">machine</span> !
+                </h1>
+                
+                <p class="text-slate-600 max-w-md mt-4"> 
+                    Connectez-vous à l'aide de votre adresse mail Centrale Lille et réservez votre machine du fablab ! 
+                </p>
+            </div>
+            
+            <div>
+                <a href="rerserver.php" class="inline-block bg-white text-indigo-600 border border-indigo-600 px-8 py-3.5 rounded-xl font-semibold   hover:bg-indigo-700 hover:text-white transition-all shadow-md">
+                    Réservez dès maintenant
+                </a>
+            </div>
+        </div>
+
+        <div class="w-full max-w-md">
+            <img src="./img/machine.webp" alt="Machine" class="rounded-4xl shadow-xl w-full h-[350px] object-cover"/>
+        </div>
+    </div>
+</div>
