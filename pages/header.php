6335c986aedc018db8f9ea65fcbaaadbd1fda338
Eulalie <eulaliedegezelle@gmail.com>
2026-02-03 13:33:16 +0100

    page de connexion

--- pages/header.php
+++ pages/header.php
@@ -18,6 +18,7 @@ echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FabLab</title>
 
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="shortcut icon" href="./img/logo.png">
     
     <script src="https://cdn.tailwindcss.com"></script>
--- pages/login.php
+++ pages/login.php
@@ -1,7 +1,34 @@
 <?php
-// Si la page est appelée directement par son adresse, on redirige en passant pas la page index
+// Redirection si la page est appelée directement
 if (basename($_SERVER["PHP_SELF"]) != "index.php") {
-	header("Location:../index.php?view=login");
-	die("");
+    header("Location:../index.php?view=login");
+    die("");
 }
+
+// Chargement éventuel des cookies
+$login = valider("nom", "COOKIE");
+$prenom = valider("prenom", "COOKIE");
+$passe = valider("motdepasse", "COOKIE");
+$checked = valider("remember", "COOKIE") ? "checked" : "";
+?>
+<?php 
+echo "<div class='form-card'>";
+mkForm("controleur.php", "get", "form-connexion");
+       echo "<h1 class='text-center'>Connexion</h1>";
+       echo "Nom de famille";
+        mkInput("text", "nom", $login);
+        echo "Prénom";
+        mkInput("text", "prenom", $prenom);
+        echo "Mot de passe";
+        mkInput("password", "motdepasse", $passe) ;
+        echo "Se souvenir de moi  " ;
+        mkInput("checkbox", "remember", "1", $checked);
+        mkInput("submit", "action", "Connexion");
+
+        echo "<p class='mt-3 text-center'>";
+            echo "Pas encore inscrit ? <a href='index.php?view=inscription'>Inscrivez-vous</a>";
+        echo "</p>";
+    endForm();
+echo "</div>";
+
 ?>
