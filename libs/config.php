6dd419dc040e5a5b3471d1393d13cb4b043d3a62
Marceau <mamar123095@gmail.com>
2026-02-03 16:59:03 +0100

    Oups erreur

--- libs/config.php
+++ libs/config.php
@@ -1,6 +1,6 @@
 <?php
 $BDD_host="localhost";
 $BDD_user="mluciani";
-$BDD_password="Cacahuete1*"; // vide sous windows
+$BDD_password=""; // vide sous windows
 $BDD_base = "PINF"; //si vous avez appelez la base autrement a changer pour vous aussi
 ?>
