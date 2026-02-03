09144d8d801277fa8d66771ce7e226eb56ceedc2
Marceau <mamar123095@gmail.com>
2026-02-03 17:02:33 +0100

    test ?

--- libs/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$BDD_host="localhost";
-$BDD_user="mluciani";
-$BDD_password=""; // vide sous windows
-$BDD_base = "PINF"; //si vous avez appelez la base autrement a changer pour vous aussi
-?>
