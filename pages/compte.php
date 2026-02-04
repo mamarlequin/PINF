88d005b82b1b101204d4d4fe18409a9439360133
Alex <alexandra.quettier@ig2i.centralelille.fr>
2026-02-04 15:25:35 +0100

    J'ai créé le système des liens protégés, j'ai mis les boutons admin et superadmin et j'ai commencé à créé la page compte

---
+++ pages/compte.php
@@ -0,0 +1,4 @@
+ <div>
+                            <a href="controleur.php?action=Logout"
+                            class="bg-indigo-600 text-white px-5 py-2 rounded-3xl hover:bg-indigo-700 transition-all shadow-sm active:scale-95">Déconnexion</a>
+                        </div>
